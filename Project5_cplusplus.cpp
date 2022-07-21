d9f549f3909df03daacca25e3c7842a8a6698d88
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-07-21 10:03:19 +0300

    Add files via upload

---
+++ Project5_cplusplus.cpp
@@ -0,0 +1,46 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir. Şuara/78
+//Bu program kullanıcıdan alınan 2 sayının üzerinde dört işlem yapılmasını sağlamaktadır.
+//This program written for getting from user two numbers doing 4 basic operations.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double number1;
+    double number2;
+    double sum;
+    double distinction;
+    double multipication;
+    double division;
+
+    cout << "Please enter first number :";
+    cout << endl;
+    cin >> number1;
+
+    cout << endl;
+
+    cout << "Please enter second number:";
+    cout << endl;
+    cin >> number2;
+
+    cout << endl;
+
+    sum = number1 + number2;
+    distinction = number1 - number2;
+    multipication = number1 * number2;
+    division = number1 / number2;
+
+    cout << "Sum of entried two numbers :\t" << sum << endl;
+
+    cout << "Distinction of entried two numbers :\t" << distinction << endl;
+
+    cout << "Multipicaiton of entried two numbers :\t" << multipication << endl;
+
+    cout << "Division of entried two numbers :\t" << division << endl;
+
+    system("PAUSE");
+    return 0;
+
+}
+
